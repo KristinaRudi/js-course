1b9bcabd3d2466d51bd32c81b5b41bd1ec3762a0
Kristina Rudi <kristina-rudi@mail.ru>
2021-04-01 16:42:02 +0300

    fixed from mentor comments

--- projects/exceptions/index.js
+++ projects/exceptions/index.js
@@ -20,7 +20,7 @@ function isAllTrue(array, fn) {
   if (typeof fn !== 'function') {
     throw new Error('fn is not a function');
   }
-  if (!Array.isArray(array) || !array.length) {
+  if (!(array instanceof Array) || !array.length) {
     throw new Error('empty array');
   }
 
@@ -52,7 +52,7 @@ function isSomeTrue(array, fn) {
   if (typeof fn !== 'function') {
     throw new Error('fn is not a function');
   }
-  if (!Array.isArray(array) || !array.length) {
+  if (!(array instanceof Array) || !array.length) {
     throw new Error('empty array');
   }
 
